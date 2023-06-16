709b4be468911a4721020170f3bc3be6728bb2e6
Milton Procel M <miltonprocel@hotmail.com>
2023-06-16 16:00:03 -0500

    commi2

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Card from './card'
+
 
 function App() {
 
   return (
-    
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-  );
+    <>
+    <h1>Card</h1>
+    <Card/>
+    </>
+  )
 }
 
 

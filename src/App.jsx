87a8bdad8a63cb1687d414eb721f4707162352ff
Milton Procel M <miltonprocel@hotmail.com>
2023-06-16 16:15:16 -0500

    comitfinal

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import Card from './card'
+import Acor from './Acor'
+import { Container } from 'react-bootstrap'
 
 
 function App() {
 
   return (
-    <>
-    <h1>Card</h1>
-    <Card/>
-    </>
+    
+    <Container>
+      <h1 className='d-flex justify-content-center pt-5'>Como esta profe</h1>
+      <Acor/>
+      <Card/>
+      <h2>Milton Procel</h2>
+      <h3 className='pb-5'>Homework Fryday</h3>
+    </Container>
+    
   )
 }
 
--- src/Card.jsx
+++ src/Card.jsx
@@ -3,8 +3,12 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 function card() {
+
+    function fryday(){
+        alert("Es viernes y el cuerpo lo Sabe")
+    }
   return (
-    <div className='bg-black'>
+    <div className='d-flex flex-column align-items-center p-5'>
          <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src="https://picsum.photos/200/300" />
             <Card.Body>
@@ -13,7 +17,7 @@ function card() {
                 Some quick example text to build on the card title and make up the
                 bulk of the card's content.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button onClick={fryday} variant="primary">Click</Button>
             </Card.Body>
         </Card>
 
